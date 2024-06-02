f1e9a38ffae30cd97b5f1639f042d57423b1e433
leiriane <162614987+leirioli@users.noreply.github.com>
2024-06-02 13:04:40 -0300

    2.2 - tipos de dados

---
+++ modulo2/aula2_questão02.py
@@ -0,0 +1,18 @@
+# 2) Transforme o seguinte algoritmo em código. Lembre-se das funções print e type que já conhecemos de forma geral.
+# Armazene o texto "o resultado é"  em uma variável, o valor 10 em outra variável, e o valor 3.5 numa terceira variável.
+# Some os valores da segunda e terceira variável e armazene em outra variável.
+# Imprima todas as variáveis na ordem de criação e imprima também seus tipos.
+
+texto = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+valor3 = valor1 + valor2
+
+print(texto)
+print(valor1)
+print(valor2)
+print(valor3)
+print(type(texto))
+print(type(valor1))
+print(type(valor2))
+print(type(valor3))
